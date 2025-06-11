f53407ca422083108e00cd56c987d2bad0322d3d
charles-lao <charles.h.lao@gmail.com>
2025-06-11 17:11:38 +1200

    Complete Lesson 81: Navigate and Pass data between Activities

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,7 +1,10 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
+using QuizAppXamarin.Activities;
 
 namespace QuizAppXamarin
 {
@@ -9,6 +12,14 @@ namespace QuizAppXamarin
     public class MainActivity : AppCompatActivity
     {
         AndroidX.AppCompat.Widget.Toolbar toolbar;
+
+        LinearLayout historyLayout;
+        LinearLayout spaceLayout;
+        LinearLayout geographyLayout;
+        LinearLayout engineeringLayout;
+        LinearLayout programmingLayout;
+        LinearLayout businessLayout;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -25,7 +36,41 @@ namespace QuizAppXamarin
             AndroidX.AppCompat.App.ActionBar actionBar = SupportActionBar;
             actionBar.SetHomeAsUpIndicator(Resource.Drawable.menuaction);
             actionBar.SetDisplayHomeAsUpEnabled(true);
+
+
+            // View Setups
+            historyLayout = (LinearLayout)FindViewById(Resource.Id.historyLayout);
+            spaceLayout = (LinearLayout)FindViewById(Resource.Id.spaceLayout);
+            geographyLayout = (LinearLayout)FindViewById(Resource.Id.geographyLayout);
+            engineeringLayout = (LinearLayout)FindViewById(Resource.Id.engineeringLayout);
+            programmingLayout = (LinearLayout)FindViewById(Resource.Id.programmingLayout);
+            businessLayout = (LinearLayout)FindViewById(Resource.Id.businessLayout);
+
+            // Click Event Handlers
+            historyLayout.Click += HistoryLayout_Click;
+            spaceLayout.Click += SpaceLayout_Click;
+            geographyLayout.Click += GeographyLayout_Click;
         }
-        
+
+        private void GeographyLayout_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(QuizDescriptionActivity));
+            intent.PutExtra("topic", "Geography");
+            StartActivity(intent);
+        }
+
+        private void SpaceLayout_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(QuizDescriptionActivity));
+            intent.PutExtra("topic", "Space");
+            StartActivity(intent);
+        }
+
+        private void HistoryLayout_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(QuizDescriptionActivity));
+            intent.PutExtra("topic", "History");
+            StartActivity(intent);
+        }
     }
 }
